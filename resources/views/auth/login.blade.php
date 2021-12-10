a844855cf7d4fe1a1e6a3d40ea1ccbbb68e8416e
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2021-12-10 11:21:22 -0700

    Se agrego el idioma es al blog

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login de usuario</title>
+    <title>Login de usuario de {{ config('app.name', 'Laravel') }}</title>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
@@ -29,29 +29,30 @@
                     @csrf
                     <div class="input-group mb-3">
                         <input type="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input id="password" type="password" placeholder="Contraseña" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                        @error('password')
+
+                        @error('email')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="password" type="password" placeholder="Contraseña" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                     <div class="row">
                         <div class="col-8">
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title>Zendero</title>
+	<title>{{ config('app.name', 'Laravel') }}</title>
 	<link rel="stylesheet" href="{{asset('css/normalize.css')}}">
 	<link rel="stylesheet" href="{{asset('css/framework.css')}}">
 	<link rel="stylesheet" href="{{asset('css/style.css')}}">
