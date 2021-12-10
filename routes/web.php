ca3be9f900ffb020f255f89da30c0f604189d406
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2021-12-10 09:35:23 -0700

    se agrego admilte al proyecto

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
     $posts = Post::latest('published_at')->get();
     return view('welcome', compact('posts'));
 });
+
+Route::get('admin', function(){
+    return view('admin.dashboard');
+});
